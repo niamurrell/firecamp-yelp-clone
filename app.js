9b5e9043759e594d8d939445a1d199dcdc9387c6
niamurrell <niamurrell@users.noreply.github.com>
2017-07-06 21:50:47 -0700

    Comment app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// Require node modules
 var express = require("express"),
     app = express()
     bodyParser = require("body-parser");
 
+// Implement node modules
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Temp data to move to db
 var campgrounds = [
   {name: "Bolton Hill", image: "http://cdn.grindtv.com/uploads/2015/02/shutterstock_242371765.jpg"},
   {name: "Lagrange Point", image: "https://www.trend-chaser.com/wp-content/uploads/sites/7/2016/11/featured-image-6.jpg"},
@@ -12,6 +15,7 @@ var campgrounds = [
   {name: "Hurdle's Lane", image: "http://www.switchbacktravel.com/sites/default/files/images/articles/Camping%20Chair%20header.jpg"}
 ];
 
+// BEGIN ROUTES
 app.get("/", function(req, res) {
   res.render("index");
 });
@@ -21,6 +25,7 @@ app.get("/campgrounds", function(req, res) {
 });
 
 app.post("/campgrounds", function(req, res) {
+  // Get submittions from form and post to Campgrounds, redirect to page
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image};
@@ -32,7 +37,7 @@ app.get("/campgrounds/new", function(req, res) {
   res.render("new");
 });
 
-
+// Run app
 app.listen(3000, function() {
   console.log("App running on localhost:3000");
 });
