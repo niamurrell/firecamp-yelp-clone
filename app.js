e9da005e0876b0f3333ff4b9cbe3351959b0005d
niamurrell <niamurrell@users.noreply.github.com>
2017-07-06 21:15:42 -0700

    Add head & footer partials, add Bootstrap

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
   res.render("index");
 })
+
+app.get("/campgrounds", function(req, res) {
+  var campgrounds = [
+    {name: "Bolton Hill", image: "http://cdn.grindtv.com/uploads/2015/02/shutterstock_242371765.jpg"},
+    {name: "Lagrange Point", image: "https://www.trend-chaser.com/wp-content/uploads/sites/7/2016/11/featured-image-6.jpg"},
+    {name: "Summit Point", image: "http://cbsnews3.cbsistatic.com/hub/i/r/2017/02/02/2b766abd-de4a-4a2e-9a9b-877e3729cccb/thumbnail/620x350/ca132eeb2e04d9d15d3b8029e7dc76da/istock-513059696.jpg"},
+    {name: "Hurdle's Lane", image: "http://www.switchbacktravel.com/sites/default/files/images/articles/Camping%20Chair%20header.jpg"}
+  ];
+
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
 
 
 app.listen(3000, function() {
