338f96cf03727bdec5b7827c3436e5b52a729533
niamurrell <niamurrell@users.noreply.github.com>
2017-07-24 22:42:47 -0700

    Add db connection & models

--- app.js
+++ app.js
@@ -1,41 +1,86 @@
 // Require node modules
 var express = require("express"),
-    app = express()
-    bodyParser = require("body-parser");
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 // Implement node modules
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost/firecamp", {useMongoClient: true});
 
-// Temp data to move to db
-var campgrounds = [
-  {name: "Bolton Hill", image: "http://cdn.grindtv.com/uploads/2015/02/shutterstock_242371765.jpg"},
-  {name: "Lagrange Point", image: "https://www.trend-chaser.com/wp-content/uploads/sites/7/2016/11/featured-image-6.jpg"},
-  {name: "Summit Point", image: "http://cbsnews3.cbsistatic.com/hub/i/r/2017/02/02/2b766abd-de4a-4a2e-9a9b-877e3729cccb/thumbnail/620x350/ca132eeb2e04d9d15d3b8029e7dc76da/istock-513059696.jpg"},
-  {name: "Hurdle's Lane", image: "http://www.switchbacktravel.com/sites/default/files/images/articles/Camping%20Chair%20header.jpg"}
-];
+// Campground Schema & Model Setup
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Temporary Campground Creation
+// Campground.create({
+//   name: "Lagrange Point",
+//   image: "https://www.trend-chaser.com/wp-content/uploads/sites/7/2016/11/featured-image-6.jpg",
+//   description: "Broad valley nestled in the peaceful gulch of the mountain range."
+// }, function(err, campground) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log("Newly created campground:");
+//     console.log(campground);
+//   }
+// });
 
 // BEGIN ROUTES
 app.get("/", function(req, res) {
-  res.render("index");
+  res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", {campgrounds: campgrounds});
+  // Pull & render database contents:
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {campgrounds: allCampgrounds});
+    }
+  })
+  // This is how to render fixed data:
+  // res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req, res) {
-  // Get submittions from form and post to Campgrounds, redirect to page
+  // Get submitions from form and create a new db document, redirect to page
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  var description = req.body.description;
+  var newCampground = {name: name, image: image, description: description};
+  Campground.create(newCampground, function(err, addedCamp) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");
 });
+
+app.get("/campgrounds/:id", function(req, res) {
+  // Find the campground with the given id in database
+  Campground.findById(req.params.id, function(err, resultCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Render SHOW template for that campground
+      res.render("show", {campground: resultCampground});
+    }
+  })
+
+})
 
 // Run app
 app.listen(4000, function() {
