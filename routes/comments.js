5c3ad74b22fc57faf7ab25e6efe805767fdb541c
niamurrell <niamurrell@users.noreply.github.com>
2017-08-01 22:16:07 -0700

    Add crUD routes for comments

--- routes/comments.js
+++ routes/comments.js
@@ -37,6 +37,39 @@ router.post("/", isLoggedIn, function (req, res) {
   })
 })
 
+// Edit a comment - route
+router.get("/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect("Back");
+    } else {
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  })
+})
+
+// Update a comment - route
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  })
+})
+
+// Delete a comment - route
+router.delete("/:comment_id", function(req, res) { 
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  })
+})
+
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
     return next();
